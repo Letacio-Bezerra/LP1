79a525c77f2d4d8bb0bc77c078e53bfdbc8fa047
Letacio-Bezerra <letaciobezerra@gmail.com>
2024-09-02 08:47:47 -0300

    Basico e Atividade 1

---
+++ Aula 1 - Math/Atividade 1/script1.js
@@ -0,0 +1,6 @@
+const numero1 = prompt("Escreva um número")
+const numero2 = prompt("Escreva mais um número")
+
+let media = (numero1 + numero2) / 2
+
+console.log(`A média é: ${Math.round(media)}`)
---
+++ Aula 1 - Math/script.js
@@ -0,0 +1,29 @@
+let numero = 4.7
+
+//Math.round arredondamento comum =<.5+ =>.49-
+console.log(Math.round(numero))
+
+//Math.ceil arredonda sempre pra cima+
+console.log(Math.ceil(numero))
+
+//Math.floor arredonda sempre pra baixo-
+console.log(Math.floor(numero))
+
+//Math.pow gera potenciação o 1º é a base e o 2º a elevação
+console.log(Math.pow(2,3))
+
+//Math.sqrt gera raiz quadrada
+console.log(Math.sqrt(35))
+
+//Math.abs gera o número absoluto positivo (modulo)
+console.log(Math.abs(-5))
+
+//Math.min mostra o valor menor
+console.log(Math.min(7,45,89))
+
+//Math.max mostra o valor maior
+console.log(Math.max(7,45,89))
+
+//Retorna um número aleatorio de 0 a 10
+let numeroAleatorio = Math.floor(Math.random()) * 11
+console.log(numeroAleatorio)
