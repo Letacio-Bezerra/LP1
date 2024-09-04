259a8bbd4edf739bc9d9ec765dd7ceddc5a94520
Letacio-Bezerra <letaciobezerra@gmail.com>
2024-09-04 12:50:15 -0300

    Atividades 2, 3, 4

---
+++ Aula 1 - Math/Atividade 2/script2.js
@@ -0,0 +1,6 @@
+const numeroB = prompt("Escreva a base") //B = Base
+const numeroP = prompt("Escreva a potencia") //P = Potencia
+
+let resultado = Math.pow(numeroB,numeroP)
+
+alert(resultado)
---
+++ Aula 1 - Math/Atividade 3/script3.js
@@ -0,0 +1,10 @@
+const numero1 = parseInt(prompt("Escreva um número"))
+const numero2 = parseInt(prompt("Escreva mais um número"))
+const numero3 = parseInt(prompt("Escreva mais um número"))
+const numero4 = parseInt(prompt("Escreva mais um número"))
+
+let resultadoMax = Math.max(numero1,numero2,numero3,numero4)
+let resultadoMin = Math.min(numero1,numero2,numero3,numero4)
+
+alert(resultadoMax)
+alert(resultadoMin)
